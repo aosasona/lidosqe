c03fd2ed1adc3da73a6b0906c0fa470c99c5afd7
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-04-27 02:17:44 +0100

    Setup UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { IoChevronForwardSharp } from "react-icons/io5";
+
+type History = {
+	type: "input" | "error" | "result";
+	value: string;
+};
+
+type Query = {
+	sql: string;
+	args: (string | number | boolean)[];
+};
+
+const API_URL = "http://localhost:8080";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [input, setInput] = useState<string>("");
+	const [history, setHistory] = useState<History[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		if (input === "") return;
+		let currentSQL = input;
+		setHistory((prev) => [...prev, { type: "input", value: input }]);
+		setInput("");
+
+		if (currentSQL.endsWith(";")) {
+			currentSQL = currentSQL.slice(0, -1);
+		}
+
+		const parts = currentSQL?.trim()?.split("~");
+		const sql = (parts?.[0] ?? "").trim();
+		let args = parts?.[1]?.split(",") ?? [];
+		args = args.map((arg) => arg.trim());
+
+		const query: Query = { sql, args };
+		console.log(query);
+	}
+
+	function handleQuery() {
+		return;
+	}
+
+	return (
+		<main className="w-screen h-screen bg-black flex flex-col items-center justify-center">
+			<div className="h-5/6 w-[80vw] bg-zinc-900/80 space-y-1 rounded-lg overflow-x-hidden p-4">
+				{history.map((item, index) => (
+					<HistoryLog item={item} key={index} />
+				))}
+				<form className="flex items-center gap-2" onSubmit={handleSubmit}>
+					<IoChevronForwardSharp className="inline-block text-sm text-zinc-500" />
+					<input
+						type="text"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						className="w-full bg-transparent text-base font-mono placeholder:text-zinc-800 focus:outline-none"
+						placeholder="select * from users"
+					/>
+				</form>
+			</div>
+			<button className="text-red-400 hover:opacity-50 transition-all py-2 px-4" onClick={() => setHistory([])}>
+				clear
+			</button>
+		</main>
+	);
 }
 
-export default App
+function HistoryLog({ item }: { item: History }) {
+	return (
+		<div className="flex items-center gap-2">
+			{item.type == "input" ? <IoChevronForwardSharp className="inline-block text-sm text-zinc-500" /> : null}
+			<p
+				className={`font-mono text-base ${
+					item.type == "error" ? "text-red-500 font-semibold" : item.type == "result" ? "text-emerald-500 font-medium" : "text-zinc-50"
+				}`}>
+				{item.value}
+			</p>
+		</div>
+	);
+}
+
+export default App;
